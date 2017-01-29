3b47473f13d4d7f749ca6636793ea2b515f72b80
Robin Aggleton <robin.aggleton@cern.ch>
2017-01-29 13:04:09 +0000

    Update test script for py3

--- test-root.py
+++ test-root.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 import ROOT
 
-print "This is a test or PyROOT"
+print("This is a test of PyROOT")
 h = ROOT.TH1D("h", "", 10, 0, 10)
 h.FillRandom("gaus", 100)
-print h.GetMean()
+print(h.GetMean())
